4331f1646dd587446ce3d6dfe2168405b289503e
Burner <net.cheteram@gmail.com>
2025-11-18 04:45:39 +0300

    deploy api

--- team-3-fe/src/app/App.tsx
+++ team-3-fe/src/app/App.tsx
@@ -3,10 +3,7 @@ import { ErrorBoundary } from './providers/AppErrorBoundary/AppErrorBoundary';
 import { NotificationProvider } from './providers/NotificationProvider';
 import { pdfjs } from 'react-pdf';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url,
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = '/assets/pdf.worker.min.mjs';
 
 function App() {
   return (
