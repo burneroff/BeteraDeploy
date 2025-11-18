1dbca1ba12d59eb9c500d76ae2474b4a47984266
Burner <net.cheteram@gmail.com>
2025-11-18 14:25:00 +0300

    deploy api

--- team-3-fe/src/app/App.tsx
+++ team-3-fe/src/app/App.tsx
@@ -3,7 +3,8 @@ import { ErrorBoundary } from './providers/AppErrorBoundary/AppErrorBoundary';
 import { NotificationProvider } from './providers/NotificationProvider';
 import { pdfjs } from 'react-pdf';
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc =
+  'https://cdn.jsdelivr.net/npm/pdf.worker@1.0.0/pdf.worker.min.js';
 
 function App() {
   return (
